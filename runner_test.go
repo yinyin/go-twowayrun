d7cd29c5239fb30923ef1e8361398d4d8f873831
Yinyin L <yinyinl@gmail.com>
2017-12-31 17:26:25 +0800

    add invoke count check

--- runner_test.go
+++ runner_test.go
@@ -5,22 +5,28 @@ import (
 )
 
 type mockRunner1 struct {
+	countForward  int
 	errOfForward  error
+	countBackward int
 	errOfBackward error
 }
 
 func newMockRunner1(errOfForward, errOfBackward error) (r *mockRunner1) {
 	return &mockRunner1{
+		countForward:  0,
 		errOfForward:  errOfForward,
+		countBackward: 0,
 		errOfBackward: errOfBackward,
 	}
 }
 
 func (r *mockRunner1) RunForward() (err error) {
+	r.countForward++
 	return r.errOfForward
 }
 
 func (r *mockRunner1) RunBackward() (err error) {
+	r.countBackward++
 	return r.errOfBackward
 }
 
@@ -32,10 +38,68 @@ func newMockRunners1(size int) TwoWayRunners {
 	return result
 }
 
+func checkMockRunners1BothRunned(runners TwoWayRunners, t *testing.T, toIdx int) {
+	for idx, runner := range runners {
+		m1 := runner.(*mockRunner1)
+		if idx <= toIdx {
+			if 1 != m1.countForward {
+				t.Errorf("forward no run (index=%d)", idx)
+			}
+			if 1 != m1.countBackward {
+				t.Errorf("backward no run (index=%d)", idx)
+			}
+		} else {
+			if 0 != m1.countForward {
+				t.Errorf("forward runned (index=%d)", idx)
+			}
+			if 0 != m1.countBackward {
+				t.Errorf("backward runned (index=%d)", idx)
+			}
+		}
+	}
+}
+
+func checkMockRunners1ForwardRunned(runners TwoWayRunners, t *testing.T, toIdx int) {
+	for idx, runner := range runners {
+		m1 := runner.(*mockRunner1)
+		if idx <= toIdx {
+			if 1 != m1.countForward {
+				t.Errorf("forward no run (index=%d)", idx)
+			}
+		} else {
+			if 0 != m1.countForward {
+				t.Errorf("forward runned (index=%d)", idx)
+			}
+		}
+		if 0 != m1.countBackward {
+			t.Errorf("backward runned (index=%d)", idx)
+		}
+	}
+}
+
+func checkMockRunners1BackwardRunned(runners TwoWayRunners, t *testing.T, toIdx int) {
+	for idx, runner := range runners {
+		m1 := runner.(*mockRunner1)
+		if 0 != m1.countForward {
+			t.Errorf("forward runned (index=%d)", idx)
+		}
+		if idx <= toIdx {
+			if 1 != m1.countBackward {
+				t.Errorf("backward no run (index=%d)", idx)
+			}
+		} else {
+			if 0 != m1.countBackward {
+				t.Errorf("backward runned (index=%d)", idx)
+			}
+		}
+	}
+}
+
 func TestTwoWayRunners_Run_e0(t *testing.T) {
-	m1 := newMockRunners1(5)
-	err := m1.Run()
+	m1x := newMockRunners1(5)
+	err := m1x.Run()
 	if nil != err {
 		t.Errorf("Expect fully success")
 	}
+	checkMockRunners1ForwardRunned(m1x, t, 4)
 }
