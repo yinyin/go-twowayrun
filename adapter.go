535e2f0e0c3b689ecf5e782fd9bf3df3f4b5aefd
Yinyin L <yinyinl@gmail.com>
2018-01-10 01:21:58 +0800

    add context as part of runner invoke parameter

--- adapter.go
+++ adapter.go
@@ -1,7 +1,18 @@
 package twowayrun
 
+import (
+	"context"
+)
+
 // TwoWayRunner defines required methods for a two-way runner.
 type TwoWayRunner interface {
-	RunForward() (err error)
-	RunBackward() (err error)
+	// RunForward performs operation forward to commit.
+	// Nil should be returned if operation is successful.
+	// Otherwise an error should be returned.
+	RunForward(ctx context.Context) (err error)
+
+	// RunBackward performs operation rollbacks forward operation.
+	// Nil should be returned if operation is successful.
+	// Otherwise an error should be returned.
+	RunBackward(ctx context.Context) (err error)
 }
