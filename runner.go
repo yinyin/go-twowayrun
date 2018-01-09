736f12d8801eb882f3b8e9d8ab864826ebca5fb3
Yinyin L <yinyinl@gmail.com>
2018-01-10 01:22:53 +0800

    add context as parameter of Run* methods

--- runner.go
+++ runner.go
@@ -1,6 +1,7 @@
 package twowayrun
 
 import "log"
+import "context"
 
 // TwoWayRunners is a slice construct by two-way runners
 type TwoWayRunners []TwoWayRunner
@@ -18,11 +19,12 @@ func (r TwoWayRunners) Append(runner TwoWayRunner) TwoWayRunners {
 
 // Run invokes RunForward() method of runners in TwoWayRunners instance one by one.
 // The invocation will turn backward if any runner returns error.
-func (r TwoWayRunners) Run() (err error) {
+// Context object ctx will be pass into RunForward and RunBackward methods of runners.
+func (r TwoWayRunners) Run(ctx context.Context) (err error) {
 	var prevError error
 	stoppedAt := (int)(-1)
 	for idx, runner := range r {
-		e := runner.RunForward()
+		e := runner.RunForward(ctx)
 		if nil != e {
 			prevError = e
 			stoppedAt = idx
@@ -35,7 +37,7 @@ func (r TwoWayRunners) Run() (err error) {
 	}
 	for idx := stoppedAt; idx >= 0; idx-- {
 		runner := r[idx]
-		e := runner.RunBackward()
+		e := runner.RunBackward(ctx)
 		if nil != e {
 			log.Printf("TwoWayRunners.Run: failed at backward run: (index=%d, error=%v)", idx, e)
 		}
@@ -44,12 +46,13 @@ func (r TwoWayRunners) Run() (err error) {
 }
 
 // RunForward runs forward runners only.
+// Context object ctx will be pass into RunForward method of runners.
 // Will stop on runner which result into error if stopOnError is set to true.
 // If any error occurs the resulted error will be TwoWayRunErrors structure.
-func (r TwoWayRunners) RunForward(stopOnError bool) (err error) {
+func (r TwoWayRunners) RunForward(ctx context.Context, stopOnError bool) (err error) {
 	errInst := newTwoWayRunErrors()
 	for idx, runner := range r {
-		e := runner.RunForward()
+		e := runner.RunForward(ctx)
 		if nil != e {
 			errInst.appendRunnerError(e, idx)
 			log.Printf("TwoWayRunners.RunForward: failed: (index=%d, error=%v)", idx, e)
@@ -62,13 +65,14 @@ func (r TwoWayRunners) RunForward(stopOnError bool) (err error) {
 }
 
 // RunBackward runs backward runners only. Runners will be activate in reverse order.
+// Context object ctx will be pass into RunBackward method of runners.
 // Will stop on runner which result into error if stopOnError is set to true.
 // If any error occurs the resulted error will be TwoWayRunErrors structure.
-func (r TwoWayRunners) RunBackward(stopOnError bool) (err error) {
+func (r TwoWayRunners) RunBackward(ctx context.Context, stopOnError bool) (err error) {
 	errInst := newTwoWayRunErrors()
 	for idx := len(r) - 1; idx >= 0; idx-- {
 		runner := r[idx]
-		e := runner.RunBackward()
+		e := runner.RunBackward(ctx)
 		if nil != e {
 			errInst.appendRunnerError(e, idx)
 			log.Printf("TwoWayRunners.RunBackward: failed: (index=%d, error=%v)", idx, e)
